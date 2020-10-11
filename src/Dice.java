01035bf899d2df4ae31635bedf0b907e09269ab0
Lucas Skals <skalslucas@gmail.com>
2020-10-11 23:31:24 +0200

    Commit 2 Dice.java and Main.java

--- src/Dice.java
+++ src/Dice.java
@@ -1,16 +1,40 @@
 public class Dice {
 
-    private final int MAX = 6;
-
+    private final int diceFaces = 6;
+    private final int numberOfDices = 2;
     private int faceValue;
 
 
 
-    setDice
+   public Dice()
+           // sets a constructor for the object Dice.
+        {
+            faceValue = 0;
+        }
 
-    getDice
+        public int roll()
+                // method that generates a random value for faceValue contingent on diceFaces and numberofDices.
+        {
+            faceValue = (int)(Math.random()*diceFaces) + 1;
+            faceValue = faceValue*numberOfDices;
+            return faceValue;
+        }
 
+        public void setFaceValue(int value)
+        {
+            if (value > 0 && value <= diceFaces*numberOfDices)
+                faceValue = value;
+        }
 
+        public int getFaceValue()
+        {
+            return faceValue;
+        }
 
+        public String toString()
+        {
+            String result = Integer.toString(faceValue);
+            return result;
+        }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+
+
+    public static void main(String[] args){
+        Dice dice = new Dice();
+        dice.roll();
+        System.out.println(dice.toString());
+    }
+
+
+
 }
