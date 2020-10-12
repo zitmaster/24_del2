0e4801a633718d169bee57a1d5686aa6cc01252d
Lucas Skals <skalslucas@gmail.com>
2020-10-12 23:54:05 +0200

    Commit 6 updated Player.java (by Lucas)

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,24 @@
 public class Player {
+    private static int currentPlayer = 0;
+    private static final int playerAmount = 2;
 
-    public Player{
-
+    public Player
+    {
     }
 
+    // sets current player at the beginning of the round
+    public void nextPlayer()
+    {
+        currentPlayer = (currentPlayer + 1) % playerAmount;
+    }
 
+    public void setCurrentPlayer(int currentPlayer)
+    {
+        Player.currentPlayer = currentPlayer;
+    }
 
+    public int getCurrentPlayer()
+    {
+        return currentPlayer;
+    }
 }
