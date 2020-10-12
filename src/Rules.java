00b860f55b8d5e27e28f7b6e5324963bbcdd4d51
Lucas Skals <skalslucas@gmail.com>
2020-10-13 00:51:03 +0200

    Commit 13 updated Rules.java (by Lucas)

--- src/Rules.java
+++ src/Rules.java
@@ -5,6 +5,7 @@ public class Rules {
     public void gameRules()
     {
         Account account = new Account();
+        //ends the game if the current player's account balance reaches 3000 or above.
         if (account.getPlayerAccountBalance() >= 3000)
         {
             endGame(); // need to implement in Game.java class
