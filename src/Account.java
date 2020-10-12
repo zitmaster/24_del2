6ce37c61dab8db69fb7b95cdd2d5119bbf043a26
Lucas Skals <skalslucas@gmail.com>
2020-10-13 00:41:03 +0200

    Commit 8 updated Account.java (by Lucas)

--- src/Account.java
+++ src/Account.java
@@ -1,10 +1,10 @@
-/*public class Account{
-    private final int accountStartBalance = 1000;
+public class Account{
     private int player1Score = 1000;
     private int player2Score = 1000;
 
     //test method below//
-    /*private int x;
+    /*
+    private int x;
     public static int i;
     public static int j;
 
@@ -15,18 +15,44 @@
         {
             account.setPlayer1AccountBalance(5);
 
-        System.out.println(account.getPlayer1AccountBalance());
+        System.out.println("Player 1 "+account.getPlayer1AccountBalance());
+            System.out.println("Player 2 "+account.getPlayer1AccountBalance());
+
         }
     }
-
-    public void setPlayer1AccountBalance(int x)
+*/
+    public void setPlayer1AccountBalance(int x) // private int x to be replaced with getGameBoardScore
     {
-        this.player1Score = x + player1Score;
+        Player p = new Player();
+
+        switch (p.getCurrentPlayer())
+        {
+            case 0:
+                this.player1Score = x + player1Score; //replace x with GetMethod from GameBoard
+                break;
+            case 1:
+                this.player2Score = x + player2Score; //replace x with GetMethod from GameBoard
+                break;
+            default:
+                System.out.println("invalid playerscore");
+        }
     }
 
     public int getPlayer1AccountBalance()
     {
-        return this.player1Score;
+        Player p = new Player();
+        int y = 0;
+        switch (p.getCurrentPlayer())
+        {
+            case 0:
+                y = this.player1Score; //replace y with GetMethod from GameBoard
+            break;
+            case 1:
+                y = this.player2Score; //replace y with GetMethod from GameBoard
+            break;
+            default:
+                System.out.println("invalid playerscore");
+        }
+        return y;
     }
 }
-*/
