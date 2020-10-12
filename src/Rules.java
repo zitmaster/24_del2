befd7674264599de2646c4f3a90a95b20292316c
Lucas Skals <skalslucas@gmail.com>
2020-10-12 22:25:47 +0200

    Commit 4 updated Rules.java and Player.java (by Lucas)

--- src/Rules.java
+++ src/Rules.java
@@ -4,7 +4,7 @@ public class Rules {
 
     public static void gameRules()
     {
-        if (player.getScore >= 3000)
+        if (Player.getScore >= 3000)
         {
             endGame();
         }
