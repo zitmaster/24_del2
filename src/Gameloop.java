28c1ee4c03684a6d16e9a608e2032fe700a6cbb2
zitmaster <Victorcebula@yahoo.dk>
2020-10-19 20:34:54 +0200

    Made a scanner in Gameloop-class

--- src/Gameloop.java
+++ src/Gameloop.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Gameloop {
+
+    private static String input;
+
+
+    private static void scanner(){
+        Scanner scan = new Scanner(System.in);
+        input = scan.nextLine();
+    }
+
+    private static void gameloop(){
+
+
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,3 @@
-/*public class Main {
-
-
-    public static void main(String[] args){
-        Dice dice = new Dice();
-
-        System.out.println(dice.getFaceValue());
-    }
-
-
+public class Main {
 
 }
-*/
