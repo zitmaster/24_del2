8c0794954097733851014683413fb9a1ef06b2d7
Lucas Skals <skalslucas@gmail.com>
2020-10-21 23:44:35 +0200

    changed setters and getters for Account

--- src/Account.java
+++ src/Account.java
@@ -1,18 +1,15 @@
 public class Account{
     private int player1Score = 1000;
     private int player2Score = 1000;
-    private int currentPlayer;
     private int gameBoardScore;
 
     // constructor for Account object.
-    public Account(int updateScore, int currentPlayer)
+    public Account()
     {
-        this.currentPlayer = currentPlayer; //current player comes from game.java.
-        this.gameBoardScore = updateScore; //updateScore comes from game.java
     }
 
     //test method below//
-
+    /*
     public static int i;
     public static void main(String[] args){
         Account account = new Account();
@@ -25,39 +22,21 @@ public class Account{
             System.out.println("Player 2 "+account.getPlayerAccountBalance());
         }
     }
+*/
 
-
-    public void setPlayerAccountBalance(int gameBoardScore, int currentPlayer) // in Game.java account.setPlayer1AccountBalance(getMethod from GameBaord)
-    {
-        int c = currentPlayer;
-        switch (c)
-        {
-            case 0:
-                this.player1Score = gameBoardScore + player1Score; //replace x with GetMethod from GameBoard
-                break;
-            case 1:
-                this.player2Score = gameBoardScore + player2Score; //replace x with GetMethod from GameBoard
-                break;
-            default:
-                System.out.println("invalid playerscore");
-        }
+    public void setPlayer1Score(int gameBoardScore) {
+        this.player1Score = gameBoardScore + player1Score;
+    }
+    public void setPlayer2Score(int gameBoardScore) {
+        this.player2Score = gameBoardScore + player2Score;
     }
 
     //gets the accountBalance of the current player.
-    public int getPlayerAccountBalance(int currentPlayer)
-    {
-        int y = 0;
-        switch (currentPlayer)
-        {
-            case 0:
-                y = this.player1Score;
-            break;
-            case 1:
-                y = this.player2Score;
-            break;
-            default:
-                System.out.println("invalid playerscore");
-        }
-        return y;
+    public int getPlayer1Score() {
+        return player1Score;
     }
+    public int getPlayer2Score(){
+        return player2Score;
+    }
+
 }
