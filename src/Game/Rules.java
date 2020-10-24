672acd2acf465c6a96ada519c97ca5b125ad0b17
Lucas Skals <skalslucas@gmail.com>
2020-10-24 23:10:12 +0200

    changed rules class

--- src/Game/Rules.java
+++ src/Game/Rules.java
@@ -2,12 +2,14 @@ package Game;
 
 public class Rules {
 
-    public void gameRules(int currentAccountValue)
-    {
-        int AccountValue = currentAccountValue;
+    public void gameRules(int currentAccountValue, String currentPlayername) {
+        int accountValue = currentAccountValue;
+        String currentPlayer = currentPlayername;
         //ends the game if the current player's account balance reaches 3000 or above.
-        if (AccountValue >= 3000)
+        if (accountValue >= 3000) {
+            System.out.println(currentPlayer + " won");
             System.exit(0);
+        }
     }
 
 }
