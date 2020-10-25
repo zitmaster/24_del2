3a8a453d96ea618ec9bb03a20d3de6267b1086a3
Lucas Skals <skalslucas@gmail.com>
2020-10-25 18:04:12 +0100

    Added player objects in Player class

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,20 +1,19 @@
 package Game;
 
-
-
 public class Player {
     public static Player player1 = new Player("Player1", 1);
     public static Player player2 = new Player("Player2", 2);
 
-    private final int currentPlayerPosition = 0;
+    private final int startPlayerPosition = 0;
     private int currentPlayerPosition;
 
     public Player(String nameReference, int IdInput) {
         String name = nameReference;
         int ID = IdInput;
-        int currentPlayerPosition;
+        currentPlayerPosition = startPlayerPosition;
     }
-    public void setCurrentPlayerPosition(){
+    public void setCurrentPlayerPosition(int gameBoardInput){
+        this.currentPlayerPosition = gameBoardInput;
 
     }
 
