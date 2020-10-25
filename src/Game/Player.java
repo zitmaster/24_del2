bdb1350b4829912b6d1f8eccf5f67beea3748131
Lucas Skals <skalslucas@gmail.com>
2020-10-25 17:59:14 +0100

    Added player objects in Player class

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,6 +1,11 @@
 package Game;
 
+
+
 public class Player {
+    public static Player player1 = new Player("Player1", 1);
+    public static Player player2 = new Player("Player2", 2);
+
     private final int currentPlayerPosition = 0;
     private int currentPlayerPosition;
 
