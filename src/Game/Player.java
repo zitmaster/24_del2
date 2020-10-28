4390906882a6795e6be9f11649225a7aa25d24e1
Lucas Skals <skalslucas@gmail.com>
2020-10-28 17:18:12 +0100

    updated variables for Player class

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,16 +1,24 @@
 package Game;
 
 public class Player {
+    private static int currentPLayer = 0;
+    private static int amountOfPlayeers = 2;
+    private final int startPlayerPosition = 0;
+    private int currentPlayerPosition;
+
+
     public static Player player1 = new Player("Player1", 1);
     public static Player player2 = new Player("Player2", 2);
 
-    private final int startPlayerPosition = 0;
-    private int currentPlayerPosition;
 
     public Player(String nameReference, int IdInput) {
         String name = nameReference;
         int ID = IdInput;
         currentPlayerPosition = startPlayerPosition;
+    }
+
+    public static void nextPlayer(){
+
     }
 
     public void setCurrentPlayerPosition(int gameBoardInput){
